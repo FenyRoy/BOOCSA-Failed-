aea84311970cd7ce4bf13ad454044c89496087ef
Leos Abraham <101leosabraham@gmail.com>
2018-03-15 22:04:14 +0530

    change4

---
+++ app/src/main/java/com/ef_corp/boosca/LoginPage.java
@@ -0,0 +1,17 @@
+package com.ef_corp.boosca;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.widget.Button;
+
+
+public class LoginPage extends Activity {
+    Button button;
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+    }
+
+}
--- app/src/main/java/com/ef_corp/boosca/MainActivity.java
+++ app/src/main/java/com/ef_corp/boosca/MainActivity.java
@@ -1,11 +1,16 @@
 package com.ef_corp.boosca;
 
+import android.content.Context;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.LinearLayout;
 
 public class MainActivity extends AppCompatActivity {
+    LinearLayout Entry1;
 
     public void searchbutton(View view)
     {
@@ -31,6 +36,25 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_launcher);
+        addListenerOnButton();
+    }
 
+    public void addListenerOnButton() {
+
+        final Context context = this;
+
+        Entry1 =(LinearLayout) findViewById(R.id.launch);
+
+        Entry1.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View arg0) {
+
+                Intent intent = new Intent(context, LoginPage.class);
+                startActivity(intent);
+
+            }
+        });
     }
 }
